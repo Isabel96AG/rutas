cb607a0828f11b1534e760a6de44e891b2f1367e
Isabel96AG <71227201+Isabel96AG@users.noreply.github.com>
2020-11-03 16:48:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-fetch("/primercurso").then(function (respuesta) {
-    return repuesta.json()
-}).then(function (datos) {
-    let claseuno = "";
-    let clasedos = "";
-    for (let i = 0; i < datos.claseuno.lenght; i++) {
-        claseuno += `
-<h1>${datos.claseuno[i].nombre}</h1>
-<p>Apellidos:${datos.claseuno[i].apellidos}</p>
-<p>Edad:${datos.claseuno[i].edad}</p>`;
-    };
-    for (let i = 0; i < datos.clasedos.lenght; i++) {
-        clasedos += `
-<h1>${datos.clasedos[i].nombre}</h1>
-<p>Apellidos:${datos.clasedos[i].apellidos}</p>
-<p>Edad:${datos.clasedos[i].edad}</p>`;
+const express = require("express");
+const primercurso = require("./primercurso");
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.get("/primercurso", function (req, res) {
+    console.log(primercurso);
+    res.send(primercurso);
+});
+app.get("/primercurso/:clase", function (req, res) {
+    let clase = req.params.clase;
+    if (primercurso[clase] === claseuno && primercurso[clase] === clasedos) {
+
+        res.send(primercurso[clase]);
+    } else {
+        res.send("No existe esa clase");
     }
-    document.getElementById("div1").innerHTML = claseuno + clasedos;
+
+});
+app.get("/primercurso/:clase", function (req, res) {
+    let clase = req.params.clase;
+    if (primercurso[aula] !== undefined) {
+        res.send(primercurso[clase]);
+    } else {
+        res.send("No existe esa clase");
+    }
 });
 
-function buscar() {
-    let clase = document.getElementById("clase").value
-    fetch(`/primercurso/${clase}`).then(function (respuesta) {
-        return respuesta.json()
-    }).then(function (datos) {
-        let clases = "";
-        for (let i = 0; i < datos.lenght; i++) {
-            clases += `
-            <h1>${datos[i].nombre}</h1>
-            <p>${datos[i].apellidos}</p>}
-            <p>${datos[i].edad}</p>}`;
+app.post("/primercurso/:clase", function (req, res) {
+    let clase = req.body.clase;
+    let nombre = req.body.nombre;
+    let apellidos = req.body.apellidos;
+    let edad = req.body.edad;
+    let alumno = {
+        nombre: nombre,
+        apellidos: apellidos,
+        edad: edad,
+    };
+    primercurso[clase].push(alumno);
+    res.send(primercurso);
+});
 
 
-        }
-        document.getElementById("div1").innerHTML = clases;
-    })
-};
+app.listen(3000);
